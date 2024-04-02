5d1572c5ed624a0337191b484a2834b943cfe016
melloshadows <mustafaam003@gmail.com>
2024-04-02 12:18:49 +0200

    added icon , card design and added new features in the game

--- Deep_Questions.js
+++ Deep_Questions.js
@@ -6,29 +6,32 @@ let answerArea = document.querySelector("textarea")
 let showQuestion = document.querySelector(".show")
 let resultSection = document.querySelector("#results")
 let player = document.querySelector(".player-answer")
+let playerNumber = document.querySelector("#playernumber")
+let nameArea = document.querySelector("#namearea")
+let turn = document.querySelector(".turn")
+let nextTurn = document.querySelector(".nextturn")
 
 
 
 let theAnswers = []
-let randomIndex
-let clicked = false
 let theQuestions = [] 
-let numberOfPlayers = 0
+let thePlayers = []
+let clickedToShowQuestion = false
+let clickedPlayerAdded = false
+let voted = false
+let randomIndex
 let playersVotes = 0
+let playersnumber = 2
+let arrayIndex = 1
 
-function fading(){
-    hide(showQuestion)
-    randomIndex = Math.floor(Math.random() *(questions.length - 0) + 0);
-    cardContent.textContent = questions[randomIndex]
-    theQuestions.push(cardContent.textContent)
-    clicked = true
-    cardContent.style.display = "block"
-    cardContent.classList.add("fadein")
 
-}
-
-function flipit(object){
-    object.classList.add("flipped1")
+function flip(object){
+    if(object.classList.contains("flips")){
+        object.classList.remove("flips")
+    }
+    else{    
+        object.classList.add("flips")
+    }
 }
 
 function show(object){
@@ -39,89 +42,178 @@ function hide(object){
     object.style.display = "none"
 }
 
-function checkanswer(){
+function playeradded(){
+    clickedPlayerAdded = true
+    
+    if (checkanswer(nameArea)){
+        playerNumber.textContent = "player " + playersnumber + " name : "
+        playersnumber++
+        thePlayers.push(nameArea.value)
+        nameArea.value = ""
+    }  
+}
+
+function startquestions(){
+
+    clickedPlayerAdded = true
+
+    if(thePlayers.length < 1){
+        alert("There Must Be At Least 2 Players")
+    }
+
+
+    else if (checkanswer(nameArea)){
+        thePlayers.push(nameArea.value)
+        nameArea.value = ""
+        hide(playerNumber)
+        hide(nameArea)
+        hide(button[4])
+        hide(button[5])
+        show(card)
+        show(answerArea)
+        show(button[0])
+        turn.textContent = thePlayers[0] +"'s turn"
+        nextTurn.innerHTML = "------->  " + thePlayers[1]
+    }
+    
+}
+
+function showingQuestion(){
+    hide(showQuestion)
+    randomIndex = Math.floor(Math.random() *(questions.length - 0) + 0);
+    cardContent.textContent = questions[randomIndex]
+    theQuestions.push(cardContent.textContent)
+    clickedToShowQuestion = true
+    cardContent.style.display = "block"
+    cardContent.classList.add("fadein")
+
+    if(arrayIndex > 1){
+        flip(cardContent)
+    }
+}
+
+
+function startAgain(){
+    hide(button[2])
+    hide(button[3])
+    show(card)
+    show(answerArea)
+    show(button[0])
+    hide(button[1])
+    turn.textContent = thePlayers[0] +"'s turn"
+    nextTurn.innerHTML = "------->  " + thePlayers[1]
+    arrayIndex = 1
+    theQuestions = []
+    theAnswers = []
+    playersVotes = 0
+    button[2].textContent = "Vote To Show Results"
+
+    if (voted){
+        hide(resultSection)
+        player.innerHTML = ""
+
+    }
+}
+
+function checkanswer(area){
     let counter = 0
 
-    for(let i = 0 ; i < answerArea.value.length ; i++){
-        if(answerArea.value[i] == " "){
+    for(let i = 0 ; i < area.value.length ; i++){
+        if(area.value[i] == " "){
             counter++
         }
     }
     
-    if (clicked == false){
-        alert("you didn't even see the question, dummy!");
+
+    if (clickedToShowQuestion == false && clickedPlayerAdded == false){
+        alert("You Didn't Even See The Question, Dummy!");
         return false
     }
 
-    else if(answerArea.value.length == counter || answerArea.value == ""){
-        alert("you must submit an answer to the question");
-        return false
+    else if (area.value.length == counter || area.value == ""){
+        if(clickedPlayerAdded){
+            alert("You MUST Enter Your Name To Countinue")
+            clickedPlayerAdded = false
+            return false
+        }
+        else{
+            alert("You MUST Submit An Answer To The Question");
+            return false
+        }
+        
     }
+    clickedPlayerAdded = false
     
     return true
 }
 
 
-
-showQuestion.addEventListener("click", fading)
+button[4].addEventListener("click", playeradded)
+button[5].addEventListener("click", startquestions)
+showQuestion.addEventListener("click", showingQuestion)
 
 
 button[0].addEventListener("click", function(){
     
-    if (checkanswer() == true){
-        numberOfPlayers++
-        flipit(card)
-        flipit(showQuestion)
+    if (checkanswer(answerArea)){
+        turn.textContent = thePlayers[arrayIndex] +"'s turn"
+
+        if(arrayIndex == thePlayers.length - 1){
+            show(button[1])
+            hide(button[0])
+            nextTurn.innerHTML = ""
+        }
+
+        else{
+            nextTurn.innerHTML = "------->  " + thePlayers[arrayIndex + 1]
+        }   
+        
+        flip(card)
+        flip(showQuestion)
         hide(cardContent)
-        theAnswers.push(answerArea.value) 
+        theAnswers.push(answerArea.value)
         answerArea.value = ""
         show(showQuestion)
-        clicked = false
-
-        showQuestion.addEventListener("click", function(){
-            flipit(cardContent)
-            cardContent.textContent = questions[randomIndex]
-            show(button[1])
-        })
+        clickedToShowQuestion = false
+        arrayIndex++
+        
     }
     
 })
 
 button[1].addEventListener("click", function(){    
-    
-    if(checkanswer() == true){
+
+    if(checkanswer(answerArea)){
         theAnswers.push(answerArea.value)
+        turn.textContent = ""
+        answerArea.value = ""
+        hide(cardContent)
+        show(showQuestion)
+        clickedToShowQuestion = false
         hide(card)
         hide(answerArea)
         hide(button[0])
         hide(button[1])
-        show(button[2])
         show(button[3])
+        show(button[2])
     }
     
 })
 
 button[2].addEventListener("click", function(){
-    location.reload()
-})
-
-
-button[3].addEventListener("click", function(){
     playersVotes++
-    button[3].textContent = "Vote To Show Results (" + playersVotes + ")"
+    button[2].textContent = "Vote To Show Results (" + playersVotes + ")"
 
-    if(numberOfPlayers == playersVotes){
-        button[3].addEventListener("click", function(){
+    if(thePlayers.length == playersVotes){
+            voted = true
             hide(button[2])
-            hide(button[3])
             show(resultSection)
-            let playerNumber = 0
-            for(let i = 0 ; i < theQuestions.length ; i++){
-                playerNumber++
-                
-                player.innerHTML +=  "player " + playerNumber + " question: <br><br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +  theQuestions[i] + "<br><br><br> player " + playerNumber + " answer: <br><br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + theAnswers[i] + "<br><br><hr><br><br>"
-            }
-        })
+            for(let i = 0 ; i < theQuestions.length ; i++){                
+                player.innerHTML +=  thePlayers[i] + "'s question: <br><br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +  theQuestions[i] + "<br><br><br>" + thePlayers[i] + "'s answer: <br><br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + theAnswers[i] + "<br><br><hr><br><br>"
+
+            }    
     }
    
 })
+
+button[3].addEventListener("click", startAgain)
